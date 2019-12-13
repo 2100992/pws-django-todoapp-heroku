b435a02661a97f33e7b8a754d96c3185f12839c0
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-12-13 19:02:38 +0300

    added signals

--- tasks/signals.py
+++ tasks/signals.py
@@ -3,31 +3,45 @@ from django.dispatch import receiver
 from tasks.models import TodoItem, Category
 from collections import Counter
 
+# def new_count(model, ):
 
 @receiver(m2m_changed, sender=TodoItem.category.through)
-def task_cats_added(sender, instance, action, model, **kwargs):
-    if action != "post_add":
-        return
+def print_signal_info(sender, instance, action, model, **kwargs):
+    print()
+    print(f'sender = {sender}')
+    print(f'instance = {instance}')
+    print(f'action = {action}')
+    print(f'model = {model.__name__}')
+    print(f'kwargs')
+    for key, value in kwargs.items():
+        print(f'    key = {key}, value = {value}')
+    print()
 
-    for cat in instance.category.all():
-        slug = cat.slug
 
-        new_count = 0
-        for task in TodoItem.objects.all():
-            new_count += task.category.filter(slug=slug).count()
+# @receiver(m2m_changed, sender=TodoItem.category.through)
+# def task_cats_added(sender, instance, action, model, **kwargs):
+#     if action == "post_add":
+#         for cat in instance.category.all():
+#             slug = cat.slug
 
-        Category.objects.filter(slug=slug).update(todos_count=new_count)
+#             new_count = 0
+#             for task in TodoItem.objects.all():
+#                 new_count += task.category.filter(slug=slug).count()
+
+#             Category.objects.filter(slug=slug).update(todos_count=new_count)
 
 
 @receiver(m2m_changed, sender=TodoItem.category.through)
 def task_cats_removed(sender, instance, action, model, **kwargs):
-    if action != "post_remove":
-        return
+    if action in ["post_remove", "post_add"]:
+        cat_counter = Counter()
+        
+        for cat in Category.objects.all():
+            cat_counter[cat.slug] = 0
 
-    cat_counter = Counter()
-    for t in TodoItem.objects.all():
-        for cat in t.category.all():
-            cat_counter[cat.slug] += 1
+        for t in TodoItem.objects.all():
+            for cat in t.category.all():
+                cat_counter[cat.slug] += 1
 
-    for slug, new_count in cat_counter.items():
-        Category.objects.filter(slug=slug).update(todos_count=new_count)
+        for slug, new_count in cat_counter.items():
+            Category.objects.filter(slug=slug).update(todos_count=new_count)
